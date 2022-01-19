4d3cf380d3034f1fcd7cacfc15a71a55070d78d2
BradlyBarnacles <44625264+BradlyBarnacles@users.noreply.github.com>
2022-01-19 09:16:02 +0000

    implemented basic functionality

---
+++ README.md
@@ -0,0 +1,2 @@
+Allows you to fast forward to the point where the DVD logo comes close to hitting the corner
+
--- sketch.js
+++ sketch.js
@@ -1,48 +1,78 @@
+function mod(a, n) {
+  return (a % n) + (Math.sign(a) !== Math.sign(n) ? n : 0);
+}
 
 let img;
 function preload() {
-  img = loadImage('assets/dvdLogo.png');
+  img = loadImage("assets/dvdLogo.png");
 }
 
 function setup() {
-  createCanvas(400 + imgWidth, 400 + imgHeight);
+  let c = createCanvas(400 + imgWidth, 400 + imgHeight);
+  c.mousePressed(press);
+  c.mouseReleased(release);
+
   colorMode(HSB, 255);
-  tintColor = color(Math.random()*255, 255, 255)
+  tintColor = color(Math.random() * 255, 255, 255);
 }
 
-var t = 0;
-var xLatticePoint = 0;
-var yLatticePoint = 0;
-
+var isPlaying = false;
 var tintColor;
+
 const height = 400;
 const width = 400;
-const startX = 20;
-const startY = 30;
-const gradient = 0.5;
+var x = 0;
+var y = 0;
+var moveX = 0.5;
+var moveY = 0.5;
 const imgHeight = 37;
 const imgWidth = 64;
 
 function draw() {
-  t++;
-  let absX = startX + t * gradient;
-  let absY = startY + (1 - gradient) * t;
-
-  if (xLatticePoint !== Math.floor(absX / height)) {
-    xLatticePoint = Math.floor(absX / height)
-    tintColor = color(Math.random()*255, 255, 255)
-  }
-  if (yLatticePoint !== Math.floor(absY / width)) {
-    yLatticePoint = Math.floor(absY / width)
-    tintColor = color(Math.random()*255, 255, 255)
+  let speed = document.querySelector("#myRange").value / 2;
+  if (isPlaying) {
+    x = x + speed * moveX;
+    y = y + speed * moveY;
   }
 
+  if (x < 0 || x > height) {
+    moveX = -moveX;
+    x = (x > height ? 2 * height : 0) - x;
+    tintColor = color(Math.random() * 255, 255, 255);
+    CheckForNearHit()
+  }
+  if (y < 0 || y > width) {
+    moveY = -moveY;
+    y = (y > width ? 2 * width : 0) - y;
+    tintColor = color(Math.random() * 255, 255, 255);
+    CheckForNearHit()
+  }
 
-
-  let relX = height - Math.abs(absX % (2 * height) - height)
-  let relY = width - Math.abs(absY%(2*width)- width)
-  
   background(30);
-  tint(tintColor)
-  image(img, relX, relY, 64, 37);
+  tint(tintColor);
+  image(img, x, y, 64, 37);
 }
+
+
+function CheckForNearHit() {
+  let nextX = (moveX > 0 ? height - x : x) / Math.abs(moveX);
+  let nextY = (moveY > 0 ? width - y : y) / Math.abs(moveY);
+  if (Math.abs(nextX - nextY) < 10)
+    document.querySelector("#myRange").value = 5;
+}
+
+function press() {
+  t = 0;
+  isPlaying = false;
+  x = mouseX;
+  y = mouseY;
+}
+
+function release() {
+  isPlaying = true;
+
+  let magnitude = Math.sqrt(Math.pow(x - mouseX, 2) + Math.pow(y - mouseY, 2));
+  moveX = (x - mouseX) / magnitude;
+  moveY = (y - mouseY) / magnitude;
+}
+
